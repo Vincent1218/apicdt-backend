86d8cb381422097a7eddb85f1ced962a3e1f3516
Vincent1218 <vicentchai@hotmail.com>
2021-01-28 02:13:52 +0800

    Add onSubmit function on Register Page

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,58 +1,96 @@
+import { useState } from 'react'
 import React from 'react'
-import './css/Register.css';
+import '../components/css/Register.css';
 import logoBase from '../assets/image/yatai 10th logo-10.png';
 import logoTop1 from '../assets/image/yatai 10th logo-bian.png';
 
 const Register = () => {
+
+  const [engSchoolName, setEngSchoolName] = useState('')
+  const [chiSchoolName, setChiSchoolName] = useState('')
+  const [engTeamLeaderName, setEngTeamLeaderName] = useState('')
+  const [chiTeamLeaderName, setChiTeamLeaderName] = useState('')
+  const [teamLeaderContact, setTeamLeaderContact] = useState('')
+  const [teamLeaderEmail, setTeamLeaderEmail] = useState('')
+  const [debateTopics_1, setDebateTopics_1] = useState('')
+  const [debateTopics_2, setDebateTopics_2] = useState('')
+
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    console.log('Submit')
+
+    
+    
+    setEngSchoolName = ('')
+    setChiSchoolName = ('')
+    setEngTeamLeaderName = ('')
+    setChiTeamLeaderName = ('')
+    setTeamLeaderContact = ('')
+    setTeamLeaderEmail = ('')
+    setDebateTopics_1 = ('')
+    setDebateTopics_2 = ('')
+
+  }
   
   return (
-    <section className="logo-block header-gradient"> 
+    <section className="header-gradient"> 
       <div className="container main_block">
-        <div className="register_header">Register / 注册</div>
+        <div className="register_header">
+            <span className = "englishF"> Register / </span> <span> 注册 </span>
+        </div>
         <div className="regBlock row">
           <form className="col-md-8 col-12 regForm ">
             <div className="school container">
-              <div className="schoolPart formHeader"> Particulars of School / 学校资料</div>
+              <div className="schoolPart formHeader">
+                  <span className = "englishF"> Particulars of School / </span> <span> 学校资料 </span>
+              </div>
               <div className="row schoolPartForm">
                 <div className="mb-3 col-6">
-                  <input type="text" className="form-control " id="engSchoolName" placeholder="Name of School" />
+                  <input type="text" className="form-control englsihF" value={engSchoolName} placeholder="Name of School" onChange={(e) => setEngSchoolName(e.target.value)} />
                 </div>
                 <div className="mb-3 col-6">
-                  <input type="text" className="form-control" id="chiSchoolName" placeholder="学校名称" />
+                  <input type="text" className="form-control" value={chiSchoolName} placeholder="学校名称" onChange={(e) => setChiSchoolName(e.target.value)} />
                 </div>
               </div>
             </div>
             <div className="leader container">
-              <div className="leaderPart formHeader"> Particulars of Team Leader / 队长资料</div>
+              <div className="leaderPart formHeader">
+                  <span className = "englishF"> Particulars of Team Leader / </span> <span> 队长资料 </span>
+              </div>
               <div className="row leaderPartForm">
                 <div className="mb-3 col-6">
-                  <input type="text" className="form-control" id="engTeamLeaderName" placeholder="Name of Team Leader" />
+                  <input type="text" className="form-control" value={engTeamLeaderName} placeholder="Name of Team Leader" onChange={(e) => setEngTeamLeaderName(e.target.value)}/>
                 </div>
                 <div className="mb-3 col-6">
-                  <input type="text" className="form-control" id="chiTeamLeaderName" placeholder="队长姓名" />
+                  <input type="text" className="form-control" value={chiTeamLeaderName} placeholder="队长姓名" onChange={(e) => setChiTeamLeaderName(e.target.value)} />
                 </div>
               </div>
               <div className="mb-3">
-                <input type="text" className="form-control" id="teamLeaderContact" placeholder="Contact No. of Team Leader / 队长联络电话" />
+                <input type="text" className="form-control" value={teamLeaderContact} placeholder="队长联络电话" onChange={(e) => setTeamLeaderContact(e.target.value)}/>
               </div>
               <div className="mb-3">
-                <input type="email" className="form-control" id="teamLeaderEmail" placeholder="Email Address of Team Leader / 队长电邮地址" />
+                <input type="email" className="form-control" value={teamLeaderEmail} placeholder="队长电邮地址" onChange={(e) => setTeamLeaderEmail(e.target.value)}/>
               </div>
             </div>
             <div className="topics container">
-              <div className="debateTopics formHeader"> Debate Topics / 辩题</div>
+              <div className="ldebateTopics formHeader">
+                  <span className = "englishF"> Debate Topics / </span> <span> 辩题 </span>
+              </div>
               <div className="row debateTopicsForm">
                 <div className="col-6">
-                  <input type="text" className="form-control" id="debateTopics" placeholder="Debate Topics 1 / 辩题一" />
+                  <input type="text" className="form-control" value={debateTopics_1} placeholder="辩题一" onChange={(e) => setDebateTopics_1(e.target.value)}/>
                 </div>
                 <div className="col-6">
-                  <input type="text" className="form-control" id="debateTopics" placeholder="Debate Topics 2 / 辩题二" />
+                  <input type="text" className="form-control" value={debateTopics_2} placeholder="辩题二" onChange={(e) => setDebateTopics_2(e.target.value)}/>
                 </div>
               </div>
-              <div className="form-text remarks">Remarks: The topics submitted will be used for this tournament. </div>
+              <div className="form-text remarks englishF">Remarks: The topics submitted will be used for this tournament. </div>
               <div className="form-text remarks">备注：所提交之辩题将会作为本赛事之用 </div>
             </div>
-            <button type="submit" className="btn btn-primary">Submit / 提交</button>
+            <button onSubmit = {onSubmit} type="submit" className="btn btn-primary">
+              <span className = "englishF"> Submit / </span> <span> 提交 </span>
+            </button>
           </form>
           <div className="col-4 logo">
             <img src= {logoBase} alt="Asia-Pacific Intervarsity Chinese Debate Tournament" className="ten-logo" width="80%" />
